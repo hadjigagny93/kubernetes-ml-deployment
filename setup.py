ddd95974ff12bc5e5c3d6b877831c53fbc6d891f
hadjigagny93 <hadjigagny93@gmail.com>
2020-06-27 13:15:46 +0200

    restruct file

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='chaos',
     url='https://gitlab.com/yotta-academy/cohort-2020/projects/ml-prod-projects/chaos-3',
-    author='hadjigagny93@gmail.com - benedicte.nachampassak@gmail.com - jalandet504@gmail.com',
-    description='Working code for Yotta project ML3 - Group 3',
+    author='hadjigagny93@gmail.com',
+    description='Deploy ML model on Google Kuberntes Engine',
     packages=find_packages())
